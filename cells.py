d1067585a8f227df63b71c153896ebc873ab102a
Raphael Borun Das Gupta <git@raphael.dasgupta.ch>
2009-08-26 14:30:44 +0200

    fixed: cells intersecting the box where referenced by two different indices

--- cells.py
+++ cells.py
@@ -2,10 +2,10 @@ from numpy import array
 from numpy import arange
 
 class Cells:
-    def cell(self, position):
-        return position // self.cell_size
+    def get_cell_at_position(self, position):
+        return (position // self.cell_size) % self.cells_per_edge
     
     def __init__(self, min_cell_size, min_coord, max_coord):
         edge_length = max_coord - min_coord
-        cells_per_edge = edge_length // min_cell_size
-        self.cell_size = edge_length / cells_per_edge
+        self.cells_per_edge = edge_length // min_cell_size
+        self.cell_size = edge_length / self.cells_per_edge
